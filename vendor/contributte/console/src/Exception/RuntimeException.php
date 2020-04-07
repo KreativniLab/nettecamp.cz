0ae1093826bbde0713aae4f671a98a5fd8ff648a
Honza Cerny <hello@honzacerny.com>
2020-04-07 21:43:10 +0200

    Composer: remove vendor folder + add contributte/recaptcha

--- vendor/contributte/console/src/Exception/RuntimeException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace Contributte\Console\Exception;
-
-class RuntimeException extends \RuntimeException
-{
-
-}
--- vendor/ublaboo/mailing/src/Exception/MailingException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Ublaboo\Mailing\Exception;
-
-class MailingException extends \Exception
-{
-}
